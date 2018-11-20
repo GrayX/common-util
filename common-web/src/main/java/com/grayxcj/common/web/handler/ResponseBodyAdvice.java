4697154ae1e5da40e73b7af68bc07401157a48b4
GrayX <xianchangjian@treefinance.com.cn>
2018-11-20 19:01:44 +0800

    初始化

---
+++ common-web/src/main/java/com/grayxcj/common/web/handler/ResponseBodyAdvice.java
@@ -0,0 +1,10 @@
+package com.grayxcj.common.web.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author :GrayX
+ */
+@ControllerAdvice
+public class ResponseBodyAdvice {
+}
