0becaec57acf33780abdb5a38b820432dd47b155
GrayX <xianchangjian@treefinance.com.cn>
2018-11-22 19:28:28 +0800

    基础保存

--- common-eureka/src/main/java/com/grayxcj/common/eureka/CommonEurekaApplication.java
+++ common-eureka/src/main/java/com/grayxcj/common/eureka/CommonEurekaApplication.java
@@ -1,5 +1,7 @@
 package com.grayxcj.common.eureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -12,5 +14,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class CommonEurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommonEurekaApplication.class);
+
+        Logger logger = LoggerFactory.getLogger(CommonEurekaApplication.class);
+        logger.info("测试log=====");
+
+        for (int i = 0; i < 10; i++) {
+            logger.error("something wrong. id={}; name=Ryan-{};", i, i);
+        }
+        for (int i = 100; i < 110; i++) {
+            logger.info("something info. id={}; name=Ryan-{};", i, i);
+        }
     }
 }
